df87c09666b6bcc44841ccfd1b1218657228598d
fabriziofranco <fabrizio.franco@utec.edu.pe>
2022-06-03 23:22:58 -0500

    Done

--- src/mlp.cpp
+++ src/mlp.cpp
@@ -12,7 +12,7 @@ using namespace arma;
 
 
 class NeuralNetwork {
-    public:
+    private:
         int input_length;
         int n_hidden_layers;
         int output_length;
@@ -21,35 +21,8 @@ class NeuralNetwork {
         field<rowvec> deltas;
         field<mat> weights;
         string activation_function;
-public:
 
-    NeuralNetwork(int input_length, int n_hidden_layers, vector<int> neurons_per_layer, int output_length, string activation_function="sigm") {
-        this->input_length = input_length;
-        this->n_hidden_layers = n_hidden_layers;
-        this->output_length = output_length;
-        this->activation_function = activation_function;
-        
-        bias = field<colvec>(n_hidden_layers+1);
-        neurons_outputs = field<rowvec>(n_hidden_layers+1);
-        deltas = field<rowvec>(n_hidden_layers+1);
-        weights = field<mat>(n_hidden_layers+1);
-        
-
-        for (int i = 0; i <= n_hidden_layers; i++){
-            if(i==0){
-                weights(i) = mat(input_length, neurons_per_layer[i],fill::randu);
-                bias(i) = colvec(neurons_per_layer[i], fill::randu);
-            }
-            else if(i<n_hidden_layers){
-                weights(i) = mat(neurons_per_layer[i-1], neurons_per_layer[i],fill::randu);
-                bias(i) = colvec(neurons_per_layer[i], fill::randu);
-            }
-            else{
-                weights(i) = mat(neurons_per_layer[i-1], output_length,fill::randu);
-                bias(i) = colvec(output_length, fill::randu);
-            }    
-        }
-    }
+private:
 
     rowvec sigm(rowvec input){
         rowvec output = 1 / (1+exp(-input));
@@ -127,6 +100,45 @@ public:
         update_weights();
     }
 
+public:
+
+    NeuralNetwork(int input_length, int n_hidden_layers, vector<int> neurons_per_layer, int output_length, string activation_function="sigm") {
+
+        this->input_length = input_length;
+        this->n_hidden_layers = n_hidden_layers;
+        this->output_length = output_length;
+        this->activation_function = activation_function;
+        
+        bias = field<colvec>(n_hidden_layers+1);
+        neurons_outputs = field<rowvec>(n_hidden_layers+1);
+        deltas = field<rowvec>(n_hidden_layers+1);
+        weights = field<mat>(n_hidden_layers+1);
+        
+
+        for (int i = 0; i <= n_hidden_layers; i++){
+            if(i==0){
+                weights(i) = mat(input_length, neurons_per_layer[i],fill::randu);
+                bias(i) = colvec(neurons_per_layer[i], fill::randu);
+            }
+            else if(i<n_hidden_layers){
+                weights(i) = mat(neurons_per_layer[i-1], neurons_per_layer[i],fill::randu);
+                bias(i) = colvec(neurons_per_layer[i], fill::randu);
+            }
+            else{
+                weights(i) = mat(neurons_per_layer[i-1], output_length,fill::randu);
+                bias(i) = colvec(output_length, fill::randu);
+            }    
+        }
+    }
+
+    double fit(field<rowvec> X_train, rowvec Y_train, double alpha, int epochs){
+        return 0;
+    }
+
+    double predict(field<rowvec> X_test, rowvec Y_test){
+        return 0;
+    }
+
 };
 
 
@@ -137,28 +149,15 @@ int main(){
     arma_rng::set_seed(42);
     vector<int> capas{3, 2, 3, 2};
     NeuralNetwork mlp(5, 4, capas, 7, "tanh");
-
     // for(int i=0; i<mlp.weights.n_elem;i++){
     //     cout<<"Weight"<<i<<":\n"<<mlp.weights(i);
     // }
     rowvec input(5, fill::randu);
-    input = softmax(input);
-    input.print();
+    // input = softmax(input);
+    // input.print();
     // auto x = mlp.forward_propagation(input);
     // // x.print("Resultado forward:");
     // mlp.propagate_backward(x*1.2324);
 
-    // mat pesos(5,3, fill::randu);
-    // colvec bias(3, fill::randu);
-    //input.print("input:");
-    // input = max(0,input)
-    // input.print("input:");
-
-    // pesos.print("pesos:");
-    // bias.print("Bias:");
-    // mat c = input*pesos + bias.t();
-    // c.print("C:");
-    // c = c + bias.t();
-    // c.print("C:");
     return 0;
 }
